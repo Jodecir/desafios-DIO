163c7300f2f46540bcbf0c6f0ce075bb09e68128
Jodecir Neto <jodecirneto@gmail.com>
2020-12-23 12:45:48 -0300

    Introdução a Programação com Javascript

---
+++ DIO_Introdução a Programação com Javascript/Multiplicação Simples.js
@@ -0,0 +1,6 @@
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let total = valor1 * valor2
+console.log("PROD = " + total);
+
+//Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).   
