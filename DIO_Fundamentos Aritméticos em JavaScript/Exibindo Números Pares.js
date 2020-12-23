167e1c5294a21603259457dd13d270116b615988
Jodecir Neto <jodecirneto@gmail.com>
2020-12-23 12:41:39 -0300

    first commit

---
+++ DIO_Fundamentos Aritméticos em JavaScript/Exibindo Números Pares.js
@@ -0,0 +1,8 @@
+let numero = gets();
+let par = 2;
+while (par <= numero){
+  console.log(par);
+  par = par + 2;
+}
+
+//Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
