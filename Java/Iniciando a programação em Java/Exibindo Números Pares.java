54db12d598192ba74400849fa2559ce025babe97
Jodecir Neto <jodecirneto@gmail.com>
2021-08-23 09:11:48 -0300

    UPDATE 1/3

--- Java/Iniciando a programação em Java/Exibindo Números Pares.java
+++ Java/Iniciando a programação em Java/Exibindo Números Pares.java
@@ -5,8 +5,10 @@ public class exibidorPar {
   public static void main(String[] args) throws IOException {
     Scanner ler = new Scanner(System.in);         
     int number = ler.nextInt();
-    for (int i = 2 ; i <= number ; i=i+2) {
+    for (int i = 2 ; i <= number ; i = i + 2) {
         System.out.println(i);
     }
   }
 }
+
+//Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
