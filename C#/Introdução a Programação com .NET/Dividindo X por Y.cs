2c11e5b111321202f2fb555c84e415ade2a62687
Jodecir Neto <jodecirneto@gmail.com>
2021-09-13 14:27:36 -0300

    UPDATE 1/3

--- C#/Introdução a Programação com .NET/Dividindo X por Y.cs
+++ C#/Introdução a Programação com .NET/Dividindo X por Y.cs
@@ -17,3 +17,7 @@ class Desafio {
         }
     }
 }
+
+//Você terá o desafio de escrever um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo. Caso não for possível, mostre a mensagem “divisao impossivel” para os valores em questão.
+
+//A entrada contém um número inteiro N. Este N será a quantidade de pares de valores inteiros (X e Y) que serão lidos em seguida.
